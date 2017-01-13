b2755ad7a954ec5292cb5825a497c525ee1ebce5
EscapedDog <escapeddog@gmail.com>
2017-01-13 17:38:20 +0800

    init project

---
+++ Chronus/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Chronus
+//
+//  Created by Shao Tianchi on 2017/1/13.
+//  Copyright © 2017年 Shao Tianchi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Chronus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Chronus
+//
+//  Created by Shao Tianchi on 2017/1/13.
+//  Copyright © 2017年 Shao Tianchi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
